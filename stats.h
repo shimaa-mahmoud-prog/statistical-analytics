3d178a38329e0e9697826de86a8964c5bbc41bbf
shimaa mahmoud <mahmoudshimaa873@gmail.com>
2020-05-30 12:35:25 +0200

    Adding code outline and documentation for c1m1 Assessment

--- stats.h
+++ stats.h
@@ -2,17 +2,17 @@
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <stats.h>
+ * @brief <include deceleration and documentation>
  *
- * <Add Extended Description Here>
+ * <this file include all function prototypes  and documentation>
  *
  * @author <Add FirsName LastName>
  * @date <Add date >
@@ -21,20 +21,90 @@
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+/* Add Your Declarations and Function Comments here */
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief <print elements of unsigned array>
  *
- * <Add Extended Description Here>
+ * <ths function take unsigned array and print its elements>
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param <a> <unsigned array to be printed>
+ * @param <n> <unsigned variable refer to number of array elements >
  *
- * @return <Add Return Informaiton here>
+ * @return <void>
  */
+void print_array(unsigned char* a ,unsigned int n);
 
+/**
+ * @brief < find maximum element of unsigned array>
+ *
+ * <this function take unsigned array and number of its elements and return its maximum element >
+ *
+ * @param <a> <unsigned array to be printed>
+ * @param <n> <unsigned variable refer to number of array elements >
+ *
+ * @return <maximum element in entered array>
+ */
+unsigned char find_maximum(unsigned char* a , unsigned int n);
+
+/**
+ * @brief < find minimum element of unsigned array>
+ *
+ * <this function take unsigned array and number of its elements and return its minimum element >
+ *
+ * @param <a> <unsigned array to be printed>
+ * @param <n> <unsigned variable refer to number of array elements >
+ *
+ * @return <minimum element in entered array>
+ */
+ unsigned char find_minimum(unsigned char* a ,unsigned int n);
+
+/**
+ * @brief < find mean value of unsigned array>
+ *
+ * <this function take unsigned array and number of its elements and return its mean value >
+ *
+ * @param <a> <unsigned array to be printed>
+ * @param <n> <unsigned variable refer to number of array elements >
+ *
+ * @return <mean value of entered array>
+ */
+ float find_mean(unsigned char* a , unsigned int n);
+
+ /**
+ * @brief < sort unsigned array>
+ *
+ * <this function take unsigned array and sort its elements from largest to minimum  value using bubble sort algorithm >
+ *
+ * @param <a> <unsigned array to be printed>
+ * @param <n> <unsigned variable refer to number of array elements >
+ *
+ * @return <void>
+ */
+ void sort_array(unsigned char* a , unsigned int n);
+
+ /**
+ * @brief < find median element in unsigned array>
+ *
+ * <this function take unsigned array and number of its elements  and it will sort the array then check if number of element is even or odd then find its  median elements>
+ *
+ * @param <a> <unsigned array to be printed>
+ * @param <n> <unsigned variable refer to number of array elements >
+ *
+ * @return <mean value of entered array>
+ */
+ float find_median(unsigned char* a , unsigned int n);
+
+/**
+ * @brief < print statistics of unsigned array>
+ *
+ * <this function take unsigned array and number of its elements then print minimum , maximum , mean value , median of the entered array >
+ *
+ * @param <a> <unsigned array to be printed>
+ * @param <n> <unsigned variable refer to number of array elements >
+ *
+ * @return <void>
+ */
+ void print_statistics(unsigned char* a , unsigned int n);
 
 #endif /* __STATS_H__ */
