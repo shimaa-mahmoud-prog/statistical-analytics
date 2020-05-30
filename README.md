9e8f62505f7d17dc6fa048c9065c01cdf98edb1c
shimaa mahmoud <mahmoudshimaa873@gmail.com>
2020-05-30 12:21:44 +0200

    Creating init coursera repository

---
+++ README.md
@@ -0,0 +1,12 @@
+/* Add Author and Project Details here */
+/* 
+@brief <performs statistical analytics on a dataset>
+ *
+ * <this program Analyzes statistics for finding its maximum,minimum,
+     mean,median value in the statistics also sort it from largest
+      to minimum  >
+ *
+ * @author <shimaa mahmoud>
+ * @date <30 may 2020 >
+ *
+*/
---
+++ stats.c
@@ -0,0 +1,85 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material.
+ *
+ *****************************************************************************/
+/**
+ * @file <stats.c>
+ * @brief <performs statistical analytics on a dataset>
+ *
+ * <this program Analyzes statistics for finding its maximum,minimum,
+     mean,median value in the statistics also sort it from largest
+      to minimum  >
+ *
+ * @author <shimaa mahmoud>
+ * @date <30 may 2020 >
+ *
+ */
+
+
+
+#include <stdio.h>
+#include "stats.h"
+
+/* Size of the Data Set */
+#define SIZE (40)
+
+void main() {
+
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+
+  /* Other Variable Declarations Go Here */
+  /* Statistics and Printing Functions Go Here */
+
+
+
+}
+
+/* Add other Implementation File Code Here */
+
+void print_array(unsigned char* a ,unsigned int n)
+{
+
+}
+/**************************************************************************/
+unsigned char find_maximum(unsigned char* a ,unsigned int n)
+{
+
+}
+/**************************************************************************/
+unsigned char find_minimum(unsigned char* a ,unsigned int n)
+{
+}
+/**************************************************************************/
+ float find_mean(unsigned char* a , unsigned int n)
+{
+
+}
+/***************************************************************************/
+void sort_array(unsigned char* a , unsigned int n)
+{
+
+
+ }
+/**************************************************************************/
+float find_median(unsigned char* a , unsigned int n)
+{
+
+}
+/************************************************************************/
+void print_statistics(unsigned char* a , unsigned int n)
+{
+
+}
+/**************************************************************************/
+
+
